4c9c43446dfe2b6ce572b40cb4774fbcfa4829f0
landdaddy <hunter@landofarizona.org>
2025-11-19 14:41:52 -0700

    Fix indentation in get_leads()

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ ZONING_MIN_ACRES = {
 
 @st.cache_data(ttl=3600)
 def get_leads():
-parcels = gpd.read_file("https://maps.pinal.gov/arcgis/rest/services/Assessor/ParcelData/FeatureServer/0/query?where=1=1&outFields=*&f=geojson&resultRecordCount=50000")
-cities = gpd.read_file("https://maps.pinal.gov/arcgis/rest/services/Boundaries/Municipal_Boundaries/FeatureServer/0/query?where=1=1&outFields=*&f=geojson")
-@st.cache_data(ttl=3600)  # Refresh every hour
+    parcels = gpd.read_file("https://maps.pinal.gov/arcgis/rest/services/Assessor/ParcelData/FeatureServer/0/query?where=1=1&outFields=*&f=geojson&resultRecordCount=50000")
+    cities = gpd.read_file("https://maps.pinal.gov/arcgis/rest/services/Boundaries/Municipal_Boundaries/FeatureServer/0/query?where=1=1&outFields=*&f=geojson")
+    @st.cache_data(ttl=3600)  # Refresh every hour
 def fetch_pinal_data():
     try:
         # Pull parcels (limited to 50K for speed; full is 255K)
